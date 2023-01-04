4e025e934ad56f2014264fd2457c3e6ce6c65318
juungman <jungmin257@sookmyung.ac.kr>
2023-01-04 22:58:42 +0900

    boj/2576/홀수

--- jungmin/0x01/2576.cpp
+++ jungmin/0x01/2576.cpp
@@ -1,7 +1,23 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+
 int main()
 {
-    cout << "hello hello";
+    int sum = 0, minimum = 100, input;
+
+    for (int i = 0; i < 7; i++)
+    {
+        cin >> input;
+        if (input % 2 != 0) {
+            sum += input;
+            minimum = min(input, minimum);
+        }
+    }
+    if (sum == 0)
+        cout << "-1";
+    else
+        cout << sum << "\n" << minimum;
 }
+
+
