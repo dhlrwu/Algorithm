d54991df34397b1f6d31821827d766bcf26d37db
juungman <jungmin257@sookmyung.ac.kr>
2023-01-08 18:21:30 +0900

    boj/1919/애너그램 만들기/더 나은 풀이

--- jungmin/0x02/1919.cpp
+++ jungmin/0x02/1919.cpp
@@ -5,17 +5,15 @@ int main()
 {
     ios::sync_with_stdio(0); cin.tie(0);
     
-    int ans = 0, a1[26]{}, a2[26]{};
+    int ans = 0, a[2][26]{};
     string s1, s2;
     cin >> s1 >> s2;
 
-    for(char c : s1) a1[c-'a']++;
-    for(char c : s2) a2[c-'a']++;
+    for(char c : s1) a[0][c-'a']++;
+    for(char c : s2) a[1][c-'a']++;
 
-    for(int i = 0; i < 26; i++)
-    {   
-        if(a1[i] == a2[i]) continue; 
-        ans += abs(a1[i] - a2[i]);
+    for(int i = 0; i < 26; i++) {   
+        ans += abs(a[0][i] - a[1][i]);
     }
     cout << ans;
 }
