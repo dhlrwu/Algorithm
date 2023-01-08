6fdb8ca8a3ab0db00dfcf03da6b7bbe30715c479
juungman <jungmin257@sookmyung.ac.kr>
2023-01-08 17:05:47 +0900

    boj/3273/두 수의 합/더나은풀이

--- jungmin/0x02/3273.cpp
+++ jungmin/0x02/3273.cpp
@@ -1,29 +1,24 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int n, x, ans = 0, num[100001]{};
-bool arr[1000001]{};
+int num[100001]{};
+bool arr[2000001]{};
+
 int main()
 {
     ios::sync_with_stdio(0); cin.tie(0);
-
+    int n, x, ans = 0;
     cin >> n;
     for (int i = 1; i <= n; i++){
         cin >> num[i];    
-        arr[num[i]] = 1;
     }
     cin >> x;
 
     for (int i = 1; i <= n; i++)
     {
-        int a = x - num[i];
-        if(a <= 0 || a > 1000000 || a == num[i]) continue;
-
-        if(arr[a]) {
+        if (x - num[i] > 0 && arr[x - num[i]])
             ans++;
-            arr[a] = 0;
-            arr[num[i]] = 0;
-        }
+        arr[num[i]] = true;
     }
     cout << ans;
 }
