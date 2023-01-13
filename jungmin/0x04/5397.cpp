d5c23655391cb123446fca0c11a4159e997b281c
juungman <jungmin257@sookmyung.ac.kr>
2023-01-13 11:11:03 +0900

    boj/5397/키로거/연결리스트이용

--- jungmin/0x04/5397.cpp
+++ jungmin/0x04/5397.cpp
@@ -4,53 +4,35 @@ using namespace std;
 int main()
 {
     ios::sync_with_stdio(0); cin.tie(0);
+    list<char> str;
+    list<char>::iterator cursor;
     int t;
-    stack<char> left, right;
-
     cin >> t;
     while(t--){
         string s;
         cin >> s;
+        cursor = str.begin();
         for (char c : s)
         {
-            if(isalnum(c))  // 알파벳 숫자 
-                left.push(c);
+            if (c == '-')
+                cursor = str.erase(--cursor);
 
-            else if(c == '>')  // right -> left
-            {
-                if(!right.empty())
-                {
-                    left.push(right.top());
-                    right.pop();
-                }
-            }
+            else if(c == '>')  
+                if(cursor != str.end())
+                    cursor++;
 
             else if(c == '<')  // left -> right
             {
-                if(!left.empty())
-                {
-                    right.push(left.top());
-                    left.pop();
-                }
+                if(cursor != str.begin())
+                    cursor--;
             }
-            else if (c == '-')
-            {
-                if(!left.empty())
-                    left.pop();
-            }
+            else 
+                str.insert(cursor, c);
         }
         
-        while(!left.empty())
-        {
-            right.push(left.top());
-            left.pop();
-        }
-        while(!right.empty())
-        {
-            cout << right.top(); 
-            right.pop();
-        }
+        for (char c : str)
+            cout << c;
         cout << '\n';
+        str.clear();
     }
-
 }
